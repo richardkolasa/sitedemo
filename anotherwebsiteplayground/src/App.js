83e73fec7affd669feb2fa3aa7efa3c6b554ffb9
Richard Kolasa <richard.kolasa@aboutobjects.com>
2017-05-04 11:43:19 -0400

    added bio and call to action. configured footer

--- anotherwebsiteplayground/src/App.js
+++ anotherwebsiteplayground/src/App.js
@@ -7,19 +7,29 @@ class App extends Component {
       <div className="App">
         <div className="main-container">
           <h1>Rich Kolasa</h1>
+          <div className="bio">
+            i'm a photographer-turned-developer with a love for new technologies, 
+            writing, and advocating for changes to the modern work environment
+          </div>
+          
           <div className="social-row">
               <a href="https://www.twitter.com/richardkolasa">
-                <button className="Social-Button">Twitter</button>
+                <button className="social-button">Twitter</button>
               </a>
               <a href="https://medium.com/@richkolasa">
-                <button className="Social-Button">Medium</button>
+                <button className="social-button">Medium</button>
               </a>
               <a href="https://www.linkedin.com/in/richardkolasa/">
-                <button className="Social-Button">LinkedIn</button>
+                <button className="social-button">LinkedIn</button>
               </a>
           </div>
-          <h2>I made this thing with <a href="https://github.com/richardkolasa/sitedemo">React</a>.</h2>
+          <div className="call-to-action">
+            reach out, let's get into it
+          </div>
         </div>
+        <footer>
+          I made this thing with <a href="https://github.com/richardkolasa/sitedemo">&nbsp;React</a>.
+        </footer>
       </div>
     );
   }
