dd54f18142d8f2d54044a70de2d2d26f807bcc95
Richard Kolasa <richard.kolasa@aboutobjects.com>
2017-05-13 13:37:45 -0400

    added dynamic heading text

--- anotherwebsiteplayground/src/App.js
+++ anotherwebsiteplayground/src/App.js
@@ -11,14 +11,26 @@ const social = {
   linkedInName: 'LinkedIn'
 }
 
-const socialButtonClass="social-button"
+const socialButtonClass='social-button'
+
+var date = new Date();
+var dayOfWeek = date.getDay();
+
+var weekdays = new Array(7);
+weekdays[0] = '#endSundayScaries';
+weekdays[1] = 'it\'s monday: aspire(and caffeinate)';
+weekdays[2] = 'tuesday';
+weekdays[3] = 'wednesday';
+weekdays[4] = 'thursday';
+weekdays[5] = 'friday';
+weekdays[6] = 'it\'s saturday, nice!';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="main-container">
-          <h1>Rich Kolasa</h1>
+        <h1>{weekdays[dayOfWeek]}</h1>
           <div className="bio">
             i'm a photographer-turned-developer with a love for new technologies, 
             writing, and advocating for changes to the modern work environment
@@ -42,7 +54,7 @@ class App extends Component {
           </div>
         </div>
         <footer>
-          I made this thing with <a href="https://github.com/richardkolasa/sitedemo">&nbsp;React</a>.
+          Check out this site's code on <a href="https://github.com/richardkolasa/sitedemo">GitHub</a>.
         </footer>
       </div>
     );
